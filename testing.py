3bf38c3813cbdd82309f6088637f4e9bd9849f1c
Niraj Patel <786nirajpatel@gmail.com>
2020-04-14 19:29:55 -0400

    Added new lesson Imported Modules

--- testing.py
+++ testing.py
@@ -1,10 +1,12 @@
+import time
+
 with open("test.txt", "a+") as file:
     file.seek(0)
     content = file.read()
     print(content)
+    time.sleep(2)
     for x in range(2):
         file.write(content)
-        
 listu = [1,2,3,4,5]
 listu.reverse()
 print(listu)
