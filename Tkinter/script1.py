d53a2d6d401b50086a7cc49c54b20c5d82b5a4d7
Niraj Patel <786nirajpatel@gmail.com>
2020-07-14 20:13:06 -0400

    Update script1.py

--- Tkinter/script1.py
+++ Tkinter/script1.py
@@ -1,12 +1,19 @@
 from tkinter import *
 
+def km_to_miles():
+    print(e1Value.get())
+    miles = float(e1Value.get())*1.6
+    t1.insert(END, miles)
+
+
 window = Tk()
 
-b1 = Button(window, text="Execute")
+b1 = Button(window, text="Execute", command=km_to_miles)
 #b1.pack()
 b1.grid(row=0, column=0)
 
-e1 = Entry(window)
+e1Value = StringVar()
+e1 = Entry(window, textvariable=e1Value)
 e1.grid(row=0, column=1)
 
 t1 = Text(window, height=1, width=20)
