ebceb6fa2e59b1b6c893a05427263989d575b073
Niraj Patel <786nirajpatel@gmail.com>
2020-10-14 02:55:08 -0400

    Lesson 1 Done

--- OpenCV/Lesson1.py
+++ OpenCV/Lesson1.py
@@ -1,4 +1,4 @@
-import cv2
+from cv2 import cv2
 
 img = cv2.imread("galaxy.jpg", 0)
 
@@ -7,6 +7,8 @@ print(img)
 print(img.shape)
 print(img.ndim)
 
-resized_img = cv2.resize(img,(1000, 500))
+resized_img = cv2.resize(img,(img.shape[1]//2, img.shape[0]//2))
 cv2.imshow("Galaxy", resized_img)
+cv2.imwrite("Galaxy Resized.jpg", resized_img)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
